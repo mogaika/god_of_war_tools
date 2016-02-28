4d2f51d359d2c894d152231c93a1c64d855c88aa
Vladimir Jigulin <mogaika@yandex.ru>
2016-02-28 19:14:10 +0300

    GoW 2 unpaking added, but gow1 Breaked

--- README.md
+++ README.md
@@ -1,22 +1,24 @@
 ### All tools is alpha (work bad, but work)
 
-# Unpacker
+# Unpacker (GoW1 + GoW2)
 Tool for unpaking part\*.pak files using info from *GODOFWAR.TOC*
+Autodetecting version of GoW
 
-**Tested only on 1-dvd version with solo part1.pak file**
+**GoW1 Tested only on 1-dvd version with part1.pak file**
+**GoW1 Tested only on 1-dvd version with part1-5.pak files**
 
 Usage: *./unpacker path_to_game_folder [path_to_store_files]*
 
 path_to_store_files default is *path_to_game_folder + /pack*
 
-# Wadreader
+# Wadreader (GoW1)
 Tool for unpaking *.wad files. Probably not unpack all.
 
 Usage: *./wadreader path_to_wad_file [path_to_store_files]*
 
 path_to_store_files default is *path_to_wad_file + _unpacked*
 
-# gfx2img
+# gfx2img (GoW1)
 Convert gfx + pal textures to png image
 
 Usage: *./gfx2img path_to_gfx_file [--pal custom_pallete_file] [--output_file_name]*
--- gof_tools/unpacker/unpacker.go
+++ gof_tools/unpacker/unpacker.go
@@ -23,7 +23,46 @@ type File struct {
 	StartSec uint32
 }
 
-func ParseTok(tok_file string) ([]*File, error) {
+const (
+	TOK_VERSION_UNKNOWN = iota
+	TOK_VERSION_GOW_1_1DVD
+	TOK_VERSION_GOW_2_1DVD
+)
+
+// Return 1 for god of war1 1-dvd
+// Return 2 for god of war2 1-dvd
+func DetectTokVersion(tok_file string) (int, error) {
+	file, err := os.OpenFile(tok_file, os.O_RDONLY, 0777)
+	if err != nil {
+		return TOK_VERSION_UNKNOWN, err
+	}
+
+	defer file.Close()
+
+	buffer := make([]byte, 4)
+	_, err = file.Read(buffer)
+	if err != nil {
+		return TOK_VERSION_UNKNOWN, err
+	}
+
+	ver := TOK_VERSION_GOW_1_1DVD
+	strend := false
+	for _, i := range buffer {
+		if i == 0 {
+			strend = true
+		} else if i < 20 || i > 127 {
+			ver = TOK_VERSION_GOW_2_1DVD
+		} else if strend {
+			ver = TOK_VERSION_GOW_2_1DVD
+			break
+		}
+	}
+
+	return ver, nil
+}
+
+// GOF 1
+func ParseTok1(tok_file string) ([]*File, error) {
 	file, err := os.OpenFile(tok_file, os.O_RDONLY, 0777)
 	if err != nil {
 		return nil, err
@@ -49,7 +88,7 @@ func ParseTok(tok_file string) ([]*File, error) {
 
 		file := &File{
 			Name:     utils.BytesToString(buffer[0:12]),
-			Pack:     binary.LittleEndian.Uint32(buffer[12:16]),
+			Pack:     0,
 			Size:     binary.LittleEndian.Uint32(buffer[16:20]),
 			StartSec: binary.LittleEndian.Uint32(buffer[20:24])}
 
@@ -57,14 +96,98 @@ func ParseTok(tok_file string) ([]*File, error) {
 	}
 }
 
+// GOF 2
+func ParseTok2(tok_file string) ([]*File, error) {
+	const SectorsInFile = (0x3FFFF800 / SectorSize)
+	file, err := os.OpenFile(tok_file, os.O_RDONLY, 0777)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	buffer := make([]byte, 4)
+
+	_, err = file.Read(buffer)
+	if err != nil {
+		return nil, err
+	}
+
+	fcount := binary.LittleEndian.Uint32(buffer)
+	maxindex := uint32(0)
+
+	buffer = make([]byte, 36)
+	files := make([]*File, 0)
+
+	for i := uint32(0); i < fcount; i++ {
+		_, err := file.Read(buffer)
+		if err != nil {
+			return nil, err
+		}
+
+		file := &File{
+			Name:     utils.BytesToString(buffer[0:24]),
+			Size:     binary.LittleEndian.Uint32(buffer[24:28]),
+			StartSec: binary.LittleEndian.Uint32(buffer[32:36])}
+
+		files = append(files, file)
+		if file.StartSec > maxindex {
+			maxindex = file.StartSec
+		}
+	}
+
+	buffer = make([]byte, 4)
+	posmap := make([]uint32, maxindex+1)
+	for i := range posmap {
+		_, err := file.Read(buffer)
+		if err != nil {
+			return nil, err
+		}
+		sz := binary.LittleEndian.Uint32(buffer)
+		posmap[i] = sz
+	}
+
+	for _, f := range files {
+		pos := posmap[f.StartSec]
+		f.StartSec = pos % SectorsInFile
+		f.Pack = pos / SectorsInFile
+	}
+
+	return files, nil
+}
+
 func getPackName(game_folder string, pack uint32) string {
 	return path.Join(game_folder, "part"+strconv.Itoa(int(pack+1))+".pak")
 }
 
-func Unpack(game_folder string, out_folder string) error {
-	files, err := ParseTok(path.Join(game_folder, "GODOFWAR.TOC"))
-	if err != nil {
-		return err
+func Unpack(game_folder string, out_folder string, version int) error {
+	var err error
+	tok_fname := path.Join(game_folder, "GODOFWAR.TOC")
+
+	os.Mkdir(out_folder, 0777)
+
+	if version == TOK_VERSION_UNKNOWN {
+		version, err = DetectTokVersion(tok_fname)
+		if err != nil {
+			return err
+		}
+		log.Printf("Detected tok version: %v\n", version)
+	}
+
+	var files []*File
+
+	switch version {
+	case TOK_VERSION_GOW_1_1DVD:
+		files, err = ParseTok1(tok_fname)
+		if err != nil {
+			return err
+		}
+	case TOK_VERSION_GOW_2_1DVD:
+		files, err = ParseTok2(tok_fname)
+		if err != nil {
+			return err
+		}
+	case TOK_VERSION_UNKNOWN:
+		return errors.New("Unknown tok version for parsing")
 	}
 
 	packsizes := make(map[uint32]uint64, 0)
@@ -80,13 +203,6 @@ func Unpack(game_folder string, out_folder string) error {
 		if f, err := os.OpenFile(getPackName(game_folder, ps), os.O_RDONLY, 0777); err == nil {
 			packpresents[ps] = true
 			f.Close()
-		}
-	}
-
-	for _, f := range files {
-		if !packpresents[f.Pack] {
-			f.StartSec += uint32(packsizes[0] / SectorSize)
-			f.Pack = 0
 		}
 	}
 
@@ -120,14 +236,27 @@ func Unpack(game_folder string, out_folder string) error {
 
 			disk.Seek(int64(f.StartSec*SectorSize), os.SEEK_SET)
 
-			log.Printf("[%.4d/%.4d] Unpaking %s (pk: %v beg:%v sz:%v)\n", i, len(files), f.Name, f.Pack+1, f.StartSec*SectorSize, f.Size)
+			log.Printf("[%.4d/%.4d] Unpaking (pk: %v beg:%.8x sz:%.8x) %s \n", i, len(files), f.Pack+1, f.StartSec*SectorSize, f.Size, f.Name)
+
 			wrtd, err := io.CopyN(fo, disk, int64(f.Size))
-			if err != nil && err != io.EOF {
+			if err == io.EOF && wrtd != int64(f.Size) {
+				log.Printf("Parted file: %.8x size: %.8x file: %#v \n", wrtd, f.Size, f)
+				curpart++
+
+				disk.Close()
+				disk, err = os.OpenFile(getPackName(game_folder, f.Pack), os.O_RDONLY, 0777)
+				if err != nil {
+					return err
+				}
+
+				next_wrtd, err := io.CopyN(fo, disk, int64(f.Size)-wrtd)
+				if err != nil || (err == io.EOF && (next_wrtd+wrtd) != int64(f.Size)) {
+					return err
+				}
+			} else if err != nil {
 				return err
-			} else if wrtd != int64(f.Size) {
-				log.Println(wrtd, f.Size)
-				return errors.New("File not copied fully")
 			}
+
 			fo.Close()
 		}
 	}
@@ -139,7 +268,7 @@ func main() {
 	flag.Parse()
 	args := flag.Args()
 
-	game := "../.."
+	game := `E:\Downloads\God of War  NTSC(USA)  PS2DVD-9\GODOFWAR_BACKUP`
 	if len(args) > 0 {
 		game = args[0]
 	}
@@ -149,7 +278,7 @@ func main() {
 		out = args[1]
 	}
 
-	if err := Unpack(game, out); err != nil {
+	if err := Unpack(game, out, TOK_VERSION_UNKNOWN); err != nil {
 		log.Fatalln("Error when unpaking: ", err)
 	}
 }
