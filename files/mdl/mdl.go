f8dbd8861a82dc309194ee545c504d3952d7be3a
Vladimir Jigulin <mogaika@yandex.ru>
2016-04-25 07:01:50 +0300

    convert models. vif reader implemented

--- files/mdl/mdl.go
+++ files/mdl/mdl.go
@@ -11,38 +11,37 @@ import (
 	"github.com/mogaika/god_of_war_tools/utils"
 )
 
-/*
-MDL
-{
-	+8		u32		mdls left in file
-	+0x50	u32		first section offset
-	SECTION
-	{
-	+2		u8		1 if present?, 0 if last?
-	+4		u32		position realtive to first section, if 0 - last?
-	}
-}
-*/
-
 type stUV struct {
 	u, v int16
 }
 
-type stXYZ struct {
+type stNorm struct {
 	x, y, z float32
 }
 
-type stFACE struct {
-	i, j, k int
+type stRGBA struct {
+	r, g, b, a float32
+}
+
+type stXYZ struct {
+	x, y, z float32
+	skip    bool
+}
+
+type stBlock struct {
+	uvs   []stUV
+	trias []stXYZ
+	norms []stNorm
+	blend []stRGBA
 }
 
 func main() {
-	Convert1(`E:\Downloads\God of War  NTSC(USA)  PS2DVD-9\unpacked\ATHN04D.WAD.ex\polySurface16564_0`)
-	Convert1(`E:\Downloads\God of War  NTSC(USA)  PS2DVD-9\unpacked\SEWR01.WAD.ex\Sewer1b_0`)
+	//Convert1(`E:\Downloads\God of War  NTSC(USA)  PS2DVD-9\unpacked\ATHN04D.WAD.ex\polySurface16564_0`)
+	//Convert1(`E:\Downloads\God of War  NTSC(USA)  PS2DVD-9\unpacked\SEWR01.WAD.ex\Sewer1b_0`)
 	Convert1(`E:\Downloads\God of War  NTSC(USA)  PS2DVD-9\unpacked\R_BRSRK2.WAD.ex\berserkBlade_0`)
 	Convert1(`E:\Downloads\God of War  NTSC(USA)  PS2DVD-9\unpacked\ARENA10.WAD.ex\Arena_0`)
 	Convert1(`E:\Downloads\God of War  NTSC(USA)  PS2DVD-9\unpacked\R_HERO0.WAD.ex\hero_0`)
-	Convert1(`E:\Downloads\God of War  NTSC(USA)  PS2DVD-9\unpacked\ATHN02A.WAD.ex\scaffoldTopFac_0`)
+	//Convert1(`E:\Downloads\God of War  NTSC(USA)  PS2DVD-9\unpacked\ATHN02A.WAD.ex\scaffoldTopFac_0`)
 	Convert1(`E:\Downloads\God of War  NTSC(USA)  PS2DVD-9\unpacked\ATHN01A.WAD.ex\nightSky_0`)
 	Convert1(`E:\Downloads\God of War  NTSC(USA)  PS2DVD-9\unpacked\R_SHELL.WAD.ex\MAI_0`)
 	Convert1(`E:\Downloads\God of War  NTSC(USA)  PS2DVD-9\unpacked\R_PERM.WAD.ex\chest_0`)
@@ -53,8 +52,129 @@ func main() {
 
 	// problems with shit:
 	//Convert1(`E:\Downloads\God of War  NTSC(USA)  PS2DVD-9\unpacked\R_LGHTN0.WAD.ex\lightningRadius_0`)
-	//Convert1(`E:\Downloads\God of War  NTSC(USA)  PS2DVD-9\unpacked\R_PERM.WAD.ex\HUD_0`)
+	Convert1(`E:\Downloads\God of War  NTSC(USA)  PS2DVD-9\unpacked\R_PERM.WAD.ex\HUD_0`)
 	//Convert1(`E:\Downloads\God of War  NTSC(USA)  PS2DVD-9\unpacked\R_MEDHD2.WAD.ex\medheadNuke_0`)
+}
+
+func VifRead1(vif []byte, debug_off uint32) (error, *stBlock) {
+	block := new(stBlock)
+	/*u32 := func(idx uint32) uint32 {
+		return binary.LittleEndian.Uint32(vif[idx : idx+4])
+	}*/
+	u16 := func(idx uint32) uint16 {
+		return binary.LittleEndian.Uint16(vif[idx : idx+2])
+	}
+	u8 := func(idx uint32) uint8 {
+		return vif[idx]
+	}
+
+	pos := uint32(0)
+
+	spaces := "     "
+
+	exit := false
+
+	for i := 0; !exit; i++ {
+		pos = ((pos + 3) / 4) * 4
+		if pos >= uint32(len(vif)) {
+			break
+		}
+
+		pk_cmd := u8(pos + 3)
+		pk_num := u8(pos + 2)
+		pk_dat1 := u8(pos)
+		pk_dat2 := u8(pos + 1)
+
+		pos += 4
+
+		if pk_cmd >= 0x60 {
+			components := ((pk_cmd >> 2) & 0x3) + 1
+			bwidth := pk_cmd & 0x3
+			widthmap := []uint32{32, 16, 8, 4} // 4 - rgb5a1 (only with 4 components
+			width := widthmap[bwidth]
+
+			blocksize := uint32(components) * ((width * uint32(pk_num)) / 8)
+
+			signed := ((pk_dat2 & (1 << 6)) >> 6) ^ 1
+			address := (pk_dat2 & (1 << 7)) >> 7
+
+			target := uint32(pk_dat1) | (uint32(pk_dat2&3) << 8)
+
+			/*
+				struct VIFCodeUnpack
+				{
+				    unsigned short addr : 10; // адрес назначения (разделен на 16)
+				    unsigned short unused : 4; // не используется
+				    unsigned short usn : 1; // знак чисел: 0 - знаковое, 1 - беззнаковое.
+				    unsigned short flg : 1; // флаг адресации данных ?.
+				    unsigned char num; // количество данных для пересылки (обычно равно кол-ву вершин в меше)
+				    unsigned char format: 4; // Тип формата данных (см. ниже)
+				    unsigned char cmd : 4; // команда этого кода (UNPACK) - 6
+				};
+			*/
+
+			if pk_cmd == 0x6d && components == 4 && width == 16 && signed != 0 {
+				// it is xyzc data
+				if block.trias == nil {
+					block.trias = make([]stXYZ, 0)
+				}
+				// GS use 12:4 fixed point format
+				// 1 << 12 = 4096
+				const delimetr = 4096.0
+
+				bp := pos
+				for i := uint8(0); i < pk_num; i++ {
+
+					x := float32(int16(u16(bp))) / delimetr
+					y := float32(int16(u16(bp+2))) / delimetr
+					z := float32(int16(u16(bp+4))) / delimetr
+					skip := (u8(bp+7) >> 4) != 0
+
+					bp += 8
+					block.trias = append(block.trias, stXYZ{x: x, y: y, z: z, skip: skip})
+				}
+			} else {
+				log.Printf("%s %.6x vif unpack: %.2x elements: %.2x components: %d type: %.2d target: %.3x sign: %d addr: %d size: %.4x",
+					spaces, debug_off+pos, pk_cmd, pk_num, components, width, target, signed, address, blocksize)
+			}
+
+			pos += blocksize
+		} else {
+			switch pk_cmd {
+			case 0:
+				log.Printf("%s %.6x nop", spaces, debug_off+pos)
+			case 01:
+				log.Printf("%s %.6x Stcycl wl=%.2x cl=%.2x", spaces, debug_off+pos, pk_dat2, pk_dat1)
+			case 05:
+				cmode := " pos "
+				/*
+							enum // Decompression modes
+					case 0:
+							{
+								Normal = 0,
+								OffsetDecompression, // would conflict with vif code
+								Difference
+							}
+				*/
+				switch pk_dat1 {
+				case 1:
+					cmode = "[pos]"
+				case 2:
+					cmode = "[cur]"
+				}
+				log.Printf("%s %.6x Stmod  mode=%s (%d)", spaces, debug_off+pos, cmode, pk_dat1)
+			case 0x14:
+				log.Printf("%s %.6x Mscall proc command", spaces, debug_off+pos)
+			case 0x30:
+				log.Printf("%s %.6x Strow  proc command", spaces, debug_off+pos)
+			default:
+				log.Printf("%s %.6x VIF command: %.2x:%.2x data: %.2x:%.2x", spaces, debug_off+pos, pk_cmd, pk_num, pk_dat1, pk_dat2)
+				exit = true
+			}
+		}
+
+	}
+	return nil, block
 }
 
 func Convert1(mdl string) error {
@@ -84,7 +204,7 @@ func Convert1(mdl string) error {
 	mdls := u32(8)
 	log.Printf("total mdls: %d", mdls)
 
-	commentStart := u32(4)
+	//	commentStart := u32(4)
 
 	ofilename := fmt.Sprintf("res/out_%s.obj", mdl_filename)
 	ofile, err := os.Create(ofilename)
@@ -92,6 +212,7 @@ func Convert1(mdl string) error {
 		log.Fatalf("Cannot create file %s: %v", ofilename, err)
 	}
 	defer ofile.Close()
+
 	vertnum := 1
 
 	for i_mdl := uint32(0); i_mdl < mdls; i_mdl++ {
@@ -100,6 +221,7 @@ func Convert1(mdl string) error {
 		datacount := uint32(u16(mbs + 2))
 		log.Printf(" current mdl: %d mbs: %x; datas: %d", i_mdl, mbs, datacount)
 
+		fmt.Fprintf(ofile, "g group_%.6x\n", mbs)
 		// data = group ?
 		// sector = object ?
 		// item = part of stream ?
@@ -115,130 +237,35 @@ func Convert1(mdl string) error {
 				t := u16(sector)
 
 				fmt.Fprintf(ofile, "o sec_%.6x\n", sector)
-				swp := false
 				log.Printf("   sector %d: %x; type: %.2x", i_sec, sector, t)
-
-				o_tvs := 0
-				o_xyzs := 0
 
 				// most of sectors is 0xE
 				if t == 0xe || t == 0x1d || t == 0x24 {
 					//t0 := u32(sector + 4)
 					//t2 := sector + 0x20
 
-					itemsCount := u32(sector+0xc) * uint32(u8(sector+0x18))
+					packetsCount := u32(sector+0xc) * uint32(u8(sector+0x18))
 
-					log.Printf("   sector items: %x", itemsCount)
-
-					for i := uint32(0); i < itemsCount; i++ {
-						item := sector + 0x20 + i*0x10
-						rep := u32(item + 4)
+					for i := uint32(0); i < packetsCount; i++ {
+						packet := sector + 0x20 + i*0x10
+						rep := u32(packet + 4)
 						newrep := rep + sector
 
 						block := newrep
+						swp := false
+						log.Printf("    packet %d; pos: %x block:%x %s", i, packet, newrep, mdl_filename)
 
-						log.Printf("    item %d; pos: %x; replace %x to %x %s", i, item, rep, newrep, mdl_filename)
+						err, vifpack := VifRead1(file[block:], block)
+						if err != nil {
+							return err
+						} else {
+							if vifpack.trias != nil && len(vifpack.trias) > 0 {
+								for i := range vifpack.trias {
+									t := &vifpack.trias[i]
 
-						sblock := block
+									fmt.Fprintf(ofile, "v %f %f %f\n\n", t.x, t.y, t.z)
 
-					blockcycle:
-						for {
-							if block >= commentStart || block+3 >= uint32(len(file)) {
-								log.Printf("       [      -%.6x] limit", block)
-								break
-							}
-
-							dattype := u8(block + 3)
-							if dattype == 0 {
-								log.Printf("       [      -%.6x] zerotype", block)
-								break
-							}
-
-							bcount := uint32(u8(block + 2))
-
-							sblock = block
-
-							switch dattype {
-							case 1:
-								block += 4
-								//curst = sblock
-								log.Printf("       [%.6x-%.6x] data %.2x      %.8x", sblock, block, dattype, u32(sblock))
-							case 5:
-								block += 4
-								log.Printf("       [%.6x-%.6x] data %.2x      %.8x", sblock, block, dattype, u32(sblock))
-							case 0x30:
-								block += 4 + 16
-								log.Printf("       [%.6x-%.6x] data %.2x      %.8x", sblock, block, dattype, u32(sblock))
-							case 0x64:
-								block += 4 + bcount*8
-								log.Printf("       [%.6x-%.6x] data b;en %.2x %.8x", sblock, block, bcount, u32(sblock))
-							case 0x65:
-								// 2ByteSignedIntegerUVcoords(U,V)
-								block += 4 + bcount*4
-								log.Printf("       [%.6x-%.6x] data uv   %.2x %.8x", sblock, block, bcount, u32(sblock))
-
-								p := sblock + 4
-								for i := uint32(0); i < bcount; i++ {
-									//_ := float32(int16(u16(p))) / 256.0
-									//_ := float32(int16(u16(p+2))) / 256.0
-									p += 4
-									o_tvs++
-								}
-
-							case 0x6a:
-								// 1ByteSignedInteger(X,Y,Z)
-								block += 4 + ((bcount*3+3)/4)*4
-								log.Printf("       [%.6x-%.6x] data wtf  %.2x %.8x", sblock, block, bcount, u32(sblock))
-							case 0x6c:
-								// unknown shit?
-								shid := u8(sblock)
-								block += 4 + bcount*0x10
-
-								algn := ""
-
-								switch shid {
-								case 0x49:
-									fallthrough
-								case 0x55:
-									fallthrough
-								case 0x9f:
-									fallthrough
-								case 0xab:
-									fallthrough
-								case 0xf4:
-									if sblock&0xf < 8 {
-										block = ((block + 0x7) / 0x8) * 0x8
-									}
-
-								case 0:
-									if bcount > 1 {
-										block = ((block + 0xf) / 0x10) * 0x10
-										algn = "bcoun"
-									}
-								default:
-									log.Fatalf("Error: new type of shit: %.2x", shid)
-								}
-
-								log.Printf("       [%.6x-%.6x] data shit %.2x %.8X shid: %.2x off al:%s",
-									sblock, block, bcount, u32(sblock), shid, algn)
-							case 0x6d:
-								// 2ByteSignedVertexes(X,Y,Z)+1ByteUnknown+1ByteCONN
-								block += 4 + bcount*8
-								log.Printf("       [%.6x-%.6x] data xyz  %.2x %.8x", sblock, block, bcount, u32(sblock))
-
-								// GS use 12:4 fixed point format
-								// 1 << 12 = 4096
-								const delimetr = 4096.0
-								for i := uint32(0); i < bcount; i++ {
-									p := sblock + 4 + i*8
-									x := float32(int16(u16(p))) / delimetr
-									y := float32(int16(u16(p+2))) / delimetr
-									z := float32(int16(u16(p+4))) / delimetr
-
-									push := u8(p+7) >> 4
-									fmt.Fprintf(ofile, "v %f %f %f\n#%x\n", x, y, z, push)
-
-									if push != 8 {
+									if !t.skip {
 										i2 := vertnum - 1
 										i3 := vertnum - 2
 										if swp {
@@ -248,81 +275,15 @@ func Convert1(mdl string) error {
 										fmt.Fprintf(ofile, "f %d %d %d\n", vertnum, i2, i3)
 									}
 									swp = !swp
-									o_xyzs++
 									vertnum++
 								}
-
-								fmt.Fprintf(ofile, "\n\n\n")
-							case 0x6e:
-								block += 4 + bcount*4
-								log.Printf("       [%.6x-%.6x] data rgba %.2x %.8x", sblock, block, bcount, u32(sblock))
-							/*case 0x3f:
-							block += 4 + bcount*8
-							log.Printf("       [%.6x-%.6x] data kek  %.2x %.8x", sblock, block, bcount, u32(sblock))*/
-							default:
-								log.Printf("  !!!!  ~~~~ UNKNOWN DATTYPE: %.6x %x[%.8x] !!!!!!!!!!!!!!!!! %s", block, dattype, u32(sblock), mdl_filename)
-								break blockcycle
+								fmt.Fprintf(ofile, "\n\n")
 							}
 						}
 					}
 				}
-
-				log.Printf("Object vts: %d xyzs: %d\n", o_tvs, o_xyzs)
 			}
 		}
 	}
-	/*
-		block := binary.LittleEndian.Uint32(buf4)
-		blockid := (block >> 24) & 0xff
-
-		size := uint32(math.MaxUint32)
-
-		switch blockid {
-		case 0x05:
-		case 0x65:
-		case 0x6c:
-		case 0x6d:
-		case 0x6e:
-		case 0xd8:
-		default:
-			if _, err := f.Read(buf4); err != nil {
-				return err
-			}
-			size = binary.LittleEndian.Uint32(buf4)
-		}
-
-		if block == 0x10001 {
-			// size not -= 4
-		} else if blockid == 0x05 {
-			size = 20
-		} else if blockid == 0x30 {
-			size -= 0x20
-			size -= 4
-		} else if blockid == 0x65 {
-			count := (block >> 16) & 0xff
-			log.Printf("Count of uv : %v\n", count)
-			size = count * 4
-		} else if blockid == 0x6d {
-			count := (block >> 16) & 0xff
-			log.Printf("Count of xyz : %v\n", count)
-			size = count*8 + 4
-		} else if blockid == 0x6e {
-			count := (block >> 16) & 0xff
-			log.Printf("Count of colorblend? : %v\n", count)
-			size = count*4 + 4
-		} else if blockid == 0x6c {
-			size = 16
-		} else {
-			size -= 4
-		}
-
-		log.Printf("block 0x%.8x [%.2x]: size 0x%.8x\n", block, blockid, size)
-
-		if _, err := f.Seek(int64(size), os.SEEK_CUR); err != nil {
-			return err
-		}*/
-
-	//log.Printf("%#v\n", head)
-
 	return nil
 }
